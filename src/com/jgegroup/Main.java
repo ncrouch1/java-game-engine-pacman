9be29291c15fe2b19c1b68eb4727f74bb965a105
Jesse <144566795+Jesse-x86@users.noreply.github.com>
2024-04-28 22:33:12 -0700

    Start doing Javadoc

--- src/com/jgegroup/Main.java
+++ src/com/jgegroup/Main.java
@@ -3,6 +3,10 @@ package com.jgegroup;
 import com.jgegroup.GameConfig.GameConfig;
 
 public class Main {
+    /**
+     * Entry point of program, pass params to game
+     * @param args arguments passed to the program
+     */
     public static void main(String[] args) {
         GameConfig.main(args);
     }
